3d79c0d9762ac6220e3b9d2fa9916f00a32b4c65
Rohit sonwane <ASUS@asus19.rohit>
2023-09-18 19:46:00 +0530

    update

--- confingDB/confingDB.js
+++ confingDB/confingDB.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
-import "dotenv/config.js"
+// import "dotenv/config.js"
+const DB_URL="mongodb+srv://rohit:rohit123@cluster0.0vl9rmn.mongodb.net/?retryWrites=true&w=majority";
  async function confingDB(){
 try {
-    await mongoose.connect(process.env.DB_URL);
+    await mongoose.connect(DB_URL);
     console.log("mongodb connected");
 } catch (error) {
     console.log("mongodb  not  connected");
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config.js"
+
 import confingDB from "./confingDB/confingDB.js";
 import router from "./Router/router.js";
 import loginRouter from "./Router/usreRouter.js";
 const app=express();
 const port=3000;
+
 app.use(express.json());
 app.use(cors());
 app.use(loginRouter)
